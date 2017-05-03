dceafb99e3569e6f51125e492eb4cbde28e135cf
Caio Fernandes <caio_fsouza@hotmail.com>
2017-05-03 16:36:41 -0300

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="home">
+    <h1>Home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$maingrey: #ff0040;
+#home{
+  h1{
+    color: $maingrey
+  }
+}
+</style>
